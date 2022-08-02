715866eef84f6a7e936eb70763fd771d2af53ed6
Arjun Singh <singharjun.biz@gmail.com>
2022-08-02 20:04:02 +0530

    :pencil:docs: add post-installation instructions

--- app/installDependencies.js
+++ app/installDependencies.js
@@ -1,5 +1,8 @@
 import { execaCommand } from "execa";
 
+// Internal dependencies
+import { highlight, info } from "./utils/inform.js";
+
 /**
  * Install npm dependencies for the scaffolded package
  * @param { Path } targetDir Target directory
@@ -12,6 +15,22 @@ export async function installDependencies(targetDir) {
     await execaCommand("npx --no husky install", {
       cwd: targetDir,
     });
+
+    info(
+      "Now, you need to initialize a git repository in your project (`git init`) and add a Github remote repo. Then, you need to create two Github repo secrets to complete the configuration:"
+    );
+
+    info(
+      `1. ${highlight(
+        "GH_TOKEN"
+      )} : This is supposed to contain a Github Personal Access Token. Learn how to make one at (https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token). Make sure to make it permissive enough (might as well check all the boxes if you are unsure).`
+    );
+
+    info(
+      `2.  ${highlight(
+        "NPM_TOKEN"
+      )} : This one is supposed to contain an npm automation token. Check out (https://docs.npmjs.com/creating-and-viewing-access-tokens/) to know how to make one.`
+    );
   } catch (e) {
     console.error(e.stderr);
     throw e;
