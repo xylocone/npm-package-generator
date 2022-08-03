44861c7c76c1e33a0c39ab92806aee1412efc545
Arjun Singh <singharjun.biz@gmail.com>
2022-08-03 08:27:15 +0530

    :art:style: add highlight

--- app/cli.js
+++ app/cli.js
@@ -7,7 +7,7 @@ import { Command } from "commander";
 import { getCurrentVersion } from "./utils/getCurrentVersion.js";
 import { installDependencies } from "./installDependencies.js";
 import { generatePackageFiles } from "./generatePackageFiles.js";
-import { info } from "./utils/inform.js";
+import { info, highlight } from "./utils/inform.js";
 import Task from "./utils/Task.js";
 import TaskRunner from "./utils/TaskRunner.js";
 
@@ -23,7 +23,9 @@ program
     const targetDir = join(process.cwd(), packageName);
 
     info(
-      "NOTE: You must make sure that the package name you provide is available on the npm registry (https://npmjs.com). If the package name you provide is taken, you won't immediately get an error, but your CI job will fail later."
+      `NOTE: You must make sure that the package name you provide is available on the npm registry at ${highlight(
+        "https://npmjs.com"
+      )}. If the package name you provide is taken, you won't immediately get an error, but your CI job will fail later.`
     );
 
     tasks.pushTask(
