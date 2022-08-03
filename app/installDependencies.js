b28eed9db0771dc13e528a5087e55c1c98563186
Arjun Singh <singharjun.biz@gmail.com>
2022-08-03 08:14:38 +0530

    :pencil:docs: add usage instruction

--- app/installDependencies.js
+++ app/installDependencies.js
@@ -17,19 +17,31 @@ export async function installDependencies(targetDir) {
     });
 
     info(
-      "Now, you need to initialize a git repository in your project (`git init`) and add a Github remote repo. Then, you need to create two Github repo secrets to complete the configuration:"
+      `Now, you need to initialize a git repository in your project (${highlight(
+        `git init`
+      )}) and add a Github remote repo. Then, you need to create two Github repo secrets to complete the configuration:`
     );
 
     info(
       `1. ${highlight(
         "GH_TOKEN"
-      )} : This is supposed to contain a Github Personal Access Token. Learn how to make one at (https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token). Make sure to make it permissive enough (might as well check all the boxes if you are unsure).`
+      )} : This is supposed to contain a Github Personal Access Token. Learn how to make one at ${highlight(
+        "https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token"
+      )}. Make sure to make it permissive enough (might as well check all the boxes if you are unsure).`
     );
 
     info(
       `2.  ${highlight(
         "NPM_TOKEN"
-      )} : This one is supposed to contain an npm automation token. Check out (https://docs.npmjs.com/creating-and-viewing-access-tokens/) to know how to make one.`
+      )} : This one is supposed to contain an npm automation token. Check out ${highlight(
+        "https://docs.npmjs.com/creating-and-viewing-access-tokens/"
+      )} to know how to make one.`
+    );
+
+    info(
+      `Whenever you make a commit, you must use ${highlight(
+        "git cz"
+      )} instead of ${highlight("git commit")}.`
     );
   } catch (e) {
     console.error(e.stderr);
